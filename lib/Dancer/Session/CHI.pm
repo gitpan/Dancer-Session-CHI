649e0d9e241dd6c8b6676fe62b73b9e078820359
Richard Simões <rsimoes@cpan.org>
2011-10-23 08:43:42 +0000

    Import of RSIMOES/Dancer-Session-CHI-v0.0.1 from CPAN.
    gitpan-cpan-distribution: Dancer-Session-CHI
    gitpan-cpan-version:      v0.0.1
    gitpan-cpan-path:         RSIMOES/Dancer-Session-CHI-v0.0.1.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

---
+++ lib/Dancer/Session/CHI.pm
@@ -0,0 +1,99 @@
+package Dancer::Session::CHI;
+
+use v5.10;
+use strict;
+use warnings;
+use utf8::all;
+use namespace::autoclean;
+use CHI;
+use Dancer::Config ();
+use Dancer::Logger ();
+use English '-no_match_vars';
+use Moose;
+use MooseX::NonMoose;
+use Scalar::Util 'blessed';
+
+extends 'Dancer::Session::Abstract';
+
+our $VERSION = 'v0.0.1'; # VERSION
+# ABSTRACT: CHI-based session backend for Dancer
+
+my $CHI; # private "class attribute"
+
+sub _config;
+
+# Pre-construction:
+before [qw/create retrieve/] => sub {
+	my ($class) = @ARG;
+	return if blessed $CHI;
+	my $options = _config->{session_CHI};
+	confess 'CHI session options not found' if not ref $options;
+	my $use_plugin = $options->{use_plugin} ? 1 : 0;
+	my $is_loaded = exists _config->{plugins}{'Cache::CHI'};
+	confess "CHI plugin requested but not loaded" if $use_plugin and not $is_loaded;
+	$CHI = do {
+		given ($use_plugin) {
+			when (1) {
+				require Dancer::Plugin::Cache::CHI;
+				Dancer::Plugin::Cache::CHI->import;
+				cache();
+			}
+			default {
+				my %options = %$options;
+				delete $options{use_plugin};
+				CHI->new(\%options);
+			}
+		}
+	};
+};
+
+# Class methods:
+
+sub create {
+	my ($class) = @ARG;
+	# Indirectly create new session by flushing:
+	my $self = $class->new;
+	$self->flush;
+	return $self;
+}
+
+sub retrieve {
+	my ($class, $session_id) = @ARG;
+	my $session = $CHI->get( 'dancer_session_' . $session_id );
+	return $session;
+}
+
+# Object methods:
+
+sub flush {
+	my ($self) = @ARG;
+	my $session_key = 'dancer_session_' . $self->id;
+	$CHI->set( $session_key => $self );
+	_debug("Session data written to $session_key.");
+	return;
+}
+
+sub destroy {
+	my ($self) = @ARG;
+	my $session_key = 'dancer_session_' . $self->id;
+	$CHI->remove($session_key);
+	_debug("Session $session_key destroyed.");
+	return;
+}
+
+# Utility functions:
+
+sub _debug {
+	my ($msg) = @ARG;
+	return Dancer::Logger::debug($msg);
+}
+
+sub _config {
+	my ($key) = @ARG;
+	return Dancer::Config::settings($key);
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/01-create.t
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::Most tests => 10;
+use Dancer::Test;
+use Dancer::Session::CHI;
+
+
+
+my $class       = 'Dancer::Session::CHI';
+my %CHI_options = ( driver => 'Memory', datastore => \ my %hash );
+throws_ok(
+	sub { Dancer::Session::CHI->create },
+	qr/CHI session options not found/,
+	'CHI session without any options throws expection'
+);
+
+Dancer::set( session_CHI => { use_plugin => 1 } );
+
+throws_ok(
+	sub { $class->create },
+	qr/CHI plugin requested but not loaded/,
+	'Requesting CHI plugin without it being loaded throws expection'
+);
+
+# Run the following tests twice, first using the plugin and then without:
+Dancer::set( plugins => { 'Cache::CHI' => \%CHI_options } );
+my $with = 'with plugin';
+for ( 1..2 ) {
+	Dancer::ModuleLoader->load(Dancer::Engine->build( session => 'CHI', Dancer::config ));
+	my $session;
+	lives_ok(
+		sub { $session = $class->create },
+		"CHI session $with created"
+	);
+	can_ok $session, qw/init create retrieve flush destroy id/;
+	isa_ok $session, $class, "&create $with yields session engine that";
+	my $sess_id = $session->id;
+	ok $sess_id, "&create $with yields valid session ID ($sess_id)";
+
+	$with = 'without plugin';
+	Dancer::set( session_CHI => \%CHI_options );
+}
