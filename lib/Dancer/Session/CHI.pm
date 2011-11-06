dbd134d433e15e86e996dc8511894df2c56fc5b0
Richard Simões <rsimoes@cpan.org>
2011-11-06 18:16:32 +0000

    Import of RSIMOES/Dancer-Session-CHI-v0.0.4 from CPAN.
    gitpan-cpan-distribution: Dancer-Session-CHI
    gitpan-cpan-version:      v0.0.4
    gitpan-cpan-path:         RSIMOES/Dancer-Session-CHI-v0.0.4.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

--- lib/Dancer/Session/CHI.pm
+++ lib/Dancer/Session/CHI.pm
@@ -1,195 +1,85 @@
 package Dancer::Session::CHI;
 
-use strict;
-use warnings;
-use base 'Dancer::Session::Abstract';
+use strictures 1;
+use utf8;
+use namespace::autoclean 0.13;
+use CHI 0.49;
+use Dancer 1.3072 qw/config debug/;
+use English '-no_match_vars';
+use Moose 2.0205;
+use MooseX::ClassAttribute 0.26;
+use MooseX::NonMoose 0.22;
+use Scalar::Util 'blessed';
 
-use Dancer::ModuleLoader;
-use Dancer::Config 'setting';
+extends 'Dancer::Session::Abstract';
 
-use version 0.77; our $VERSION = qv('0.1.0');
+our $VERSION = 'v0.0.4'; # VERSION
+# ABSTRACT: CHI-based session engine for Dancer
 
-# Package-wide cache
-my $cache;
-my %options;
+class_has _cache => (
+	is         => 'ro',
+	isa        => 'Object',
+	lazy_build => 1
+);
 
+my $class = __PACKAGE__;
 
-# ordinary subs
+# Class methods:
 
-sub cache {
-    return $cache ||= _build_cache($options{params});
-}
-
-# _call_via(subname)
-#   Checks that subname is something like Foo::Bar::provide_cache and then
-#   calls it.
-#
-sub _call_via {
-    my $via = shift;
-    my $res;
-
-    unless ($via =~ qr{\A [A-Z_a-z] [0-9A-Z_a-z]* (?: :: [0-9A-Z_a-z]+)* \z}x) {
-        die "_call_via only accepts ASCII identifiers and ::";
-    }
-
-    eval {
-        no strict 'refs';
-        $res = &$via($options{params});
-    };
-    die "Failed to call $via: $@" if $@;
-    return $res;
-}
-
-sub _build_cache {
-    if ($options{via}) {
-        $cache = _call_via($options{via})
-            || die "Failed to read cache via $options{via}";
-        #Dancer::debug(__PACKAGE__." sharing cache from {$options{via}}");
-    }
-    elsif ($options{driver}) {
-        $cache = CHI->new( driver => $options{driver}, %{$options{params}} )
-            or die "failed to create CHI cache";
-        #Dancer::debug(__PACKAGE__." created cache with driver $options{driver}");
-    }
-    else {
-        die "No via/driver option for the CHI session";
-    }
-}
-
-# class methods
-
-sub init {
-    my ($class) = @_;
-
-    $class->SUPER::init(@_);
-
-    die "CHI is needed and is not installed"
-      unless Dancer::ModuleLoader->load('CHI');
-
- 	# require either a driver or "via" in settings
-    my $via    = setting('session_via');
-	my $driver = setting('session_driver');
-	my $params = setting('session_params') || {};
-    die "You must specify session_via or session_driver for CHI sessions"
-        unless ($driver || $params);
-
-	unless ($params->{namespace}) {
-		$params->{namespace} = __PACKAGE__;
-	}
-
-    %options = (
-        via    => $via,
-        driver => $driver,
-        params => $params
-    );
-}
-
-# create and return a new session object
 sub create {
-    my ($class) = @_;
+	# Indirectly create new session by flushing:
+	my ($class) = @ARG;
+	my $self = $class->new;
+	$self->flush;
+	my $session_id = $self->id;
+	debug("Session (id: $session_id) created.");
+	return $self;
+};
 
-    my $self = Dancer::Session::CHI->new;
-    $self->flush;
-    return $self;
-}
-
-# Return the session object corresponding to the given id
 sub retrieve {
-    my ($class, $id) = @_;
-
-	return cache->get($id);
+	my ($class, $session_id) = @ARG;
+	my $session = $class->_cache->get( 'dancer_session_' . $session_id );
+	return $session;
 }
 
-# instance methods
-
-sub destroy {
-    my ($self) = @_;
-	cache->remove($self->id);
-}
+# Object methods:
 
 sub flush {
-    my $self = shift;
-	cache->set($self->id, $self);
-    return $self;
+	my ($self) = @ARG;
+	my $session_key = 'dancer_session_' . $self->id;
+	$class->_cache->set( $session_key => $self );
+	return;
 }
 
+sub destroy {
+	my ($self) = @ARG;
+	my $session_key = 'dancer_session_' . $self->id;
+	$class->_cache->remove($session_key);
+	debug("Session $session_key destroyed.");
+	return;
+}
+
+# Attribute builders:
+
+sub _build__cache {
+	my $options = config->{session_CHI};
+	confess 'CHI session options not found' if not ref $options;
+	my $use_plugin = $options->{use_plugin} ? 1 : 0;
+	my $is_loaded = exists config->{plugins}{'Cache::CHI'};
+
+	confess "CHI plugin requested but not loaded" if $use_plugin and not $is_loaded;
+	if ($use_plugin) {
+		require Dancer::Plugin::Cache::CHI;
+		Dancer::Plugin::Cache::CHI->import;
+		return cache();
+	} else {
+		my %options = %$options;
+		delete $options{use_plugin};
+		return CHI->new(\%options);
+	}
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
 1;
-__END__
-
-=pod
-
-=head1 NAME
-
-Dancer::Session::CHI - Sessions stored in a CHI-based cache
-
-=head1 VERSION
-
-0.1.0
-
-=head1 DESCRIPTION
-
-This module implements a session engine by using the L<CHI> framework.
- 
-=head1 CONFIGURATION
-
-The Dancer setting B<session> should be set to C<CHI>.
-
-You should specify a C<session_driver> and if that driver needs parameters,
-then C<session_params> too.
-
-Here is an example configuration:
-
-    session: CHI
-    session_driver: FastMmap
-    session_params: { root_dir: '/path/to/cache', cache_size: '1m' }
-
-Or perhaps:
-
-    session: CHI
-    session_driver: File
-    session_params:
-        root_dir: "/srv/testapp/cache/file"
-        depth: 2
-        expires_in: 3600
-
-or, to share via a sub that returns an already-built CHI object reference:
-
-    session: CHI
-    session_via: "Dancer::Plugin::Cache::CHI::cache"
-    session_params:
-        these: "get passed to cache()"
-
-Finally, you might want to set in session_params a C<namespace> for the
-case when you have multiple apps sharing the same cache. Session IDs might
-collide in this case. See the L<CHI> documentation for details. If unset it
-defaults to "Dancer::Session::CHI".
-
-=head1 DEPENDENCY
-
-This module depends on both L<Dancer> and L<CHI>.
-
-=head1 AUTHOR
-
-Richard Huxton <richard.huxton@gmail.com>
-
-=head1 ACKNOWLEDGEMENTS
-
-The structure of this module is mostly cut+paste work from David Precious'
-L<Dancer::Session::Storable>.
-
-=head1 SEE ALSO
-
-See L<Dancer::Session> for details about session usage in route handlers, and
-L<Dancer> for general information on the Dancer web framework.  See L<CHI>
-for details on the CHI framework.
-
-=head1 COPYRIGHT
-
-This module is copyright (c) 2011 Richard Huxton <richard.huxton@gmail.com>
-
-=head1 LICENSE
-
-This module is free software and is released under the same terms as Perl
-itself.
-
-=cut
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-use Test::More tests => 3;
-
-BEGIN {
-    use_ok( 'Dancer::Session::CHI' ) || print "Bail out!\n";
-    use_ok( 'Dancer::Session::Abstract' ) || print "Bail out!\n";
-    use_ok( 'CHI' ) || print "Bail out!\n";
-}
-
-diag( "Testing Dancer::Session:CHI $Dancer::Session::CHI::VERSION, Perl $], $^X" );
