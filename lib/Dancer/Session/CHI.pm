33e88892acd0861099444f7715aea678fd101d7a
Richard Simões <rsimoes@cpan.org>
2013-02-27 00:47:01 +0000

    Import of RSIMOES/Dancer-Session-CHI-v0.1.6 from CPAN.
    gitpan-cpan-distribution: Dancer-Session-CHI
    gitpan-cpan-version:      v0.1.6
    gitpan-cpan-path:         RSIMOES/Dancer-Session-CHI-v0.1.6.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     released

--- lib/Dancer/Session/CHI.pm
+++ lib/Dancer/Session/CHI.pm
@@ -3,70 +3,30 @@ package Dancer::Session::CHI;
 use strict;
 use warnings;
 use utf8;
-use Carp;
 use CHI;
-use Dancer qw(cookie);
+use Dancer ":syntax";
 use Dancer::Logger;
-use Dancer::Config qw(setting);
 use Dancer::ModuleLoader;
+use Dancer::Exception qw(raise);
 use File::Spec::Functions qw(rel2abs);
 use Scalar::Util qw(blessed);
 
 use base "Dancer::Session::Abstract";
 
-our $VERSION = 'v0.1.5'; # TRIAL VERSION
+our $VERSION = 'v0.1.6'; # VERSION
 # ABSTRACT: CHI-based session engine for Dancer
 
 # Class methods:
 
-sub create {
-    my ($class) = @_;
-    my $self = $class->new;
-    $self->flush;
-    Dancer::Logger->debug("Session (id: " . $self->id . " created.");
-    return $self;
-}
-
-sub retrieve {
-    my (undef, $session_id) = @_;
-    my $chi = _build_chi();
-    return $chi->get("session_$session_id")
-}
-
-# Object methods:
-
-sub flush {
-    my ($self) = @_;
-    my $chi = _build_chi();
-    my $session_id = $self->id;
-    $chi->set( "session_$session_id" => $self );
-    return $self;
-}
-
-sub purge {
-    my ($self) = @_;
-    my $chi = _build_chi();
-    $chi->purge;
-    return $self;
-}
-
-sub destroy {
-    my ($self) = @_;
-    my $session_id = $self->id;
-    my $chi = _build_chi();
-    $chi->remove("session_$session_id");
-    cookie setting("session_name") => undef;
-    Dancer::Logger->debug("Session (id: $session_id) destroyed.");
-    return $self;
-}
-
 my $chi;
-sub _build_chi {
+sub _chi {
 
-    return $chi if blessed($chi) && $chi->isa("CHI");
+    return $chi if blessed($chi);
 
     my $options = setting("session_CHI");
-    ( ref $options eq ref {} ) or croak "CHI session options not found";
+    if ( ref($options) ne "HASH" ) {
+        raise core_session => "CHI session options not found";
+    }
 
     # Don't let CHI determine the absolute path:
     if ( exists $options->{root_dir} ) {
@@ -76,19 +36,55 @@ sub _build_chi {
     my $use_plugin = delete $options->{use_plugin};
     my $is_loaded = exists setting("plugins")->{"Cache::CHI"};
     if ( $use_plugin && !$is_loaded ) {
-        croak "CHI plugin requested but not loaded";
+        raise core_session => "CHI plugin requested but not loaded";
     }
 
-    return $use_plugin
+    $chi = $use_plugin
         ? do {
             my $plugin = "Dancer::Plugin::Cache::CHI";
-            my $error_msg = "$plugin is needed and is not installed";
             unless ( Dancer::ModuleLoader->load($plugin) ) {
-                raise( core_session => $error_msg );
+                raise core_session => "$plugin is needed and is not installed";
             }
             Dancer::Plugin::Cache::CHI::cache()
         }
-        : CHI->new( %{$options} );
+        : CHI->new(%$options);
+    return $chi;
+}
+
+sub create {
+    my $self = __PACKAGE__->new->flush;
+    Dancer::Logger->debug("Session (id: " . $self->id . " created.");
+    return $self;
+}
+
+sub retrieve {
+    my (undef, $session_id) = @_;
+    return _chi->get("session_$session_id");
+}
+
+# Object methods:
+
+sub flush {
+    my ($self) = @_;
+    my $session_id = $self->id;
+    _chi->set( "session_$session_id" => $self );
+    return $self;
+}
+
+sub purge {
+    _chi->purge;
+    return;
+}
+
+#sub reset :method { goto &purge }
+
+sub destroy {
+    my ($self) = @_;
+    my $session_id = $self->id;
+    _chi->remove("session_$session_id");
+    cookies->{setting("session_name")}->expires(0);
+    Dancer::Logger->debug("Session (id: $session_id) destroyed.");
+    return $self;
 }
 
 1;
